4f93c9ac17bf98002d884e2757942a5819f93a15
jjham1 <julieham03@gmail.com>
2023-04-27 19:44:09 -0400

    updating out of date info

--- aboutScript.js
+++ aboutScript.js
@@ -1,3 +1,13 @@
 function showDiv() {
     document.getElementById('contacts').style.display = "inline";
 }
+
+document.onmouseover = function(showPage) {
+    var thisPage;
+    if (!showPage) var thisPage = window.event;
+    if (showPage.srcElement) thisPage = showPage.target;
+    else if (showPage.srcElement) thisPage = showPage.srcElement;
+    if (thisPage.nodeType == 3)
+        thisPage = thisPage.parentNode;
+    console.log(thisPage.id);
+}
